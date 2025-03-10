40afd5263120ab7208ba363320117c63cdc29d11
Huiying Cao <huiyingcao2@gmail.com>
2025-03-10 11:23:24 -0700

    wip: defines user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+
+//has a constraint can't create user without username or password
+//in SQL this is called a not null constraint
+//in MongoDB this is considred a validator, the required:
+//username and password are objects
+//do NOT TYPE require, type required
+//put the object on multiple lines as seen below
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+//first argument is user, the second is the schema
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
