d4cb2c73823092a84ff2a2ac44848d07728c3d53
Huiying Cao <huiyingcao2@gmail.com>
2025-03-10 11:00:10 -0700

    wip: adds root route

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+//dependencies
 const dotenv = require("dotenv");
-dotenv.config();
 const express = require("express");
-const app = express();
-
 const mongoose = require("mongoose");
-const methodOverride = require("method-override");
 const morgan = require("morgan");
 
+//overrides the http method
+const methodOverride = require("method-override");
+
+//configure settings
+dotenv.config();
+//initialize express app
+const app = express();
+
+
 // Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3000";
-
+const port = process.env.PORT ? process.env.PORT : "3002";
+//connect to MongoDb
 mongoose.connect(process.env.MONGODB_URI);
-
+//event listener 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
@@ -20,9 +26,16 @@ mongoose.connection.on("connected", () => {
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
-// Morgan for logging HTTP requests
+// Morgan for logging HTTP requests dev is the setting
 app.use(morgan('dev'));
 
+//tell the app to listen for HTTP requests
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
+
+//mount routes (after lines 35)
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
